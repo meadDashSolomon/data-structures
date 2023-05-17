fba943b29937ecee8109d15396d33b93468f28c8
Mead Solomon <mead93@gmail.com>
2023-05-17 15:07:56 -0500

    Implemented functional queue functionality

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -3,16 +3,29 @@ var Queue = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
+  let size = 0;
+  let first = 0;
 
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    storage[size + first] = value;
+    size++;
   };
 
   someInstance.dequeue = function() {
+    if (size === 0) {
+      return undefined;
+    }
+    let dequeuedValue = storage[first];
+    delete dequeuedValue;
+    first++;
+    size--;
+    return dequeuedValue;
   };
 
   someInstance.size = function() {
+    return size;
   };
 
   return someInstance;

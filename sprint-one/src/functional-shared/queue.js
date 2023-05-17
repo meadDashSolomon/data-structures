2513465a74fcd9af7f034b95124d476350c217d4
Mead Solomon <mead93@gmail.com>
2023-05-17 18:10:33 -0500

    Completed functional-shared

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,8 +1,36 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  let someInstance = {};
+
+  someInstance._storage = [];
+  someInstance._size = 0;
+  someInstance._first = 0;
+  _(someInstance).extend(queueMethods);
+
+  return someInstance;
 };
 
-var queueMethods = {};
+var queueMethods = {
+  enqueue: function(value) {
+    this._storage[this._size + this._first] = value;
+    this._size++;
+  },
+
+  dequeue: function() {
+    if (this._size === 0) {
+      return undefined;
+    }
+    let dequeuedValue = this._storage[this._first];
+    delete this._storage[this._first];
+    this._first++;
+    this._size--;
+    return dequeuedValue
+  },
+
+  size: function() {
+    return this._size;
+  }
+};
 
 
--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,67 @@
 var Stack = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  let someInstance = {};
+
+  someInstance._storage = [];
+  someInstance._size = 0;
+  _(someInstance).extend(stackMethods);
+
+  return someInstance;
 };
 
-var stackMethods = {};
+var stackMethods = {
+  push: function(value) {
+    this._storage[this._size] = value;
+    this._size++;
+  },
+
+  pop: function() {
+    // if (this._size === 0) {
+    //   return undefined;
+    // }
+    this._size--;
+    let lastValue = this._storage[this._size];
+    delete this._storage[this._size];
+    return lastValue;
+  },
+
+  size: function() {
+    if (this._size <= 0) {
+      return 0;
+    }
+    return this._size;
+  }
+};
 
 
+// var Stack = function() {
+//   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
+//   // but try not not reference your old code in writing the new style.
+
+//   var someInstance = {};
+//   _(someInstance).extend(stackMethods);
+
+//   someInstance._storage = {};
+//   someInstance._size = 0;
+
+//   return someInstance;
+//   };
+
+// var stackMethods = {};
+
+
+// stackMethods.push = function(value) {
+//   this._storage[this._size++] = value;
+// };
+
+// stackMethods.pop = function() {
+//   this._size && this._size--;
+//   var result = this._storage[this._size];
+
+//   delete this._storage[this._size];
+
+//   return result;
+// };
+
+// stackMethods.size = function() {
+//   return this._size;
+// };

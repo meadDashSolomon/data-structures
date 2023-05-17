d23fa3b64d1962212e7286f78e52f2c0d69816f6
Mead Solomon <mead93@gmail.com>
2023-05-17 18:44:55 -0500

    completed es6

--- sprint-one/src/es6/queue.js
+++ sprint-one/src/es6/queue.js
@@ -2,6 +2,31 @@ class Queue {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this._storage = [];
+    this._size = 0;
+    this._first = 0;
   }
 
+  enqueue(value) {
+    this._storage[this._size + this._first] = value;
+    this._size++;
+  }
+
+  dequeue() {
+    if (this._size === 0) {
+      return undefined;
+    }
+    let dequeuedValue = this._storage[this._first];
+    delete this._storage[this._first];
+    this._first++;
+    this._size--;
+    return dequeuedValue;
+  }
+
+  size() {
+    if (this._size <= 0) {
+      return 0;
+    }
+    return this._size;
+  }
 }
--- sprint-one/src/es6/stack.js
+++ sprint-one/src/es6/stack.js
@@ -2,6 +2,26 @@ class Stack {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this._storage = [];
+    this._size = 0;
   }
 
+  push(value) {
+    this._storage[this._size] = value;
+    this._size++;
+  }
+
+  pop() {
+    this._size--;
+    let lastValue = this._storage[this._size];
+    delete this._storage[this._size];
+    return lastValue;
+  }
+
+  size() {
+    if (this._size <= 0) {
+      return 0;
+    }
+    return this._size;
+  }
 }

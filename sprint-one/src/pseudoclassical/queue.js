f3bd7f76e3e440e7250ef3d691ef0c70a3c847b0
Mead Solomon <mead93@gmail.com>
2023-05-17 18:30:30 -0500

    completed pseudoclassical pattern

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,26 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this._storage = [];
+  this._size = 0;
+  this._first = 0;
 };
 
+Stack.prototype.push = function (value) {
+  this._storage[this._size + this._first] = value;
+  this._size++;
+}
 
+Stack.prototype.pop = function () {
+  if (this._size === 0) {
+    return undefined;
+  }
+  let dequeuedValue = this._storage[this._first];
+  delete this._storage[this._first];
+  this._first++;
+  this._size--;
+  return dequeuedValue;
+
+Stack.prototype.size = function () {
+  return this._size;
+}
